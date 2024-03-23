bebb879e30952fecef935f1540c8f1dd2e97dcca
sumiyaaax <wave_soul14@yahoo.co.jp>
2024-03-23 14:30:58 +0900

    MenuをmaterialUIに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Routes, // SwitchからRoutesへ変更
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 import Home from "./home/Home";
 import Menu from "./components/Menu";
@@ -13,20 +14,56 @@ import ActivityRegister from "./activity/ActivityRegister";
 import ActivityList from "./activity/ActivityList";
 import CategoryList from "./category/CategoryList";
 
+const drawerWidth = 240;
+
 function App() {
   return (
     <Router>
-      <div>
-        <Menu />
-        <Routes>
-          <Route path="/activity/register" element={<ActivityRegister />} />
-          <Route path="/activity/list" element={<ActivityList />} />
-          <Route path="/category/list" element={<CategoryList />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              行動記録アプリ
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Menu />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          <Toolbar /> {/* 追加: コンテンツがAppBar下に隠れないようにする */}
+          <Routes>
+            <Route path="/activity/register" element={<ActivityRegister />} />
+            <Route path="/activity/list" element={<ActivityList />} />
+            <Route path="/category/list" element={<CategoryList />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Box>
+      </Box>
     </Router>
   );
 }
+
+// ReactDOM.renderの呼び出し部分は変更なしでそのまま利用します。
+ReactDOM.render(<App />, document.getElementById("root"));
+
 
 export default App;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Drawer, List, ListItemButton, ListItemText } from '@mui/material';
+
+const drawerWidth = 240;
 
 const Menu: React.FC = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">ホーム</Link></li>
-        <li><Link to="/activity/register">イベント登録</Link></li>
-        <li><Link to="/activity/list">イベント一覧</Link></li>
-        <li><Link to="/category/list">カテゴリー一覧</Link></li>
-      </ul>
-    </nav>
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <List>
+        <ListItemButton component={Link} to="/">
+          <ListItemText primary="ホーム" />
+        </ListItemButton>
+        <ListItemButton component={Link} to="/activity/register">
+          <ListItemText primary="イベント登録" />
+        </ListItemButton>
+        <ListItemButton component={Link} to="/activity/list">
+          <ListItemText primary="イベント一覧" />
+        </ListItemButton>
+        <ListItemButton component={Link} to="/category/list">
+          <ListItemText primary="カテゴリー一覧" />
+        </ListItemButton>
+      </List>
+    </Drawer>
   );
 };
 
